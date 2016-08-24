6ac1d04b1b309287137192face6c62822c196edf
Marc Enschede <marc@marcenschede.nl>
2016-08-24 10:39:36 +0200

    Added list of EU countries

---
+++ src/main/java/app/domain/invoice/internal/EuCountry.java
@@ -0,0 +1,34 @@
+package app.domain.invoice.internal;
+
+public enum EuCountry {
+
+    BE, // Belgium
+    FR, // France
+    IT, // Italy
+    LU, // Luxembourg
+    NL, // The Netherlands
+    DE, // Germany
+    DK, // Denemarken
+    IE, // Ireland
+    GB, // VerenigdKoninkrijk"
+    GR, // Greece
+    PT, // Portugal
+    ES, // Spain
+    FI, // Finland
+    AT, // Oostenrijk
+    SE, // Sweden
+    CY, // Cyprus
+    EE, // Estland"
+    HU, // Hongarije
+    LV, // Letland
+    LT, // Litouwen
+    MT, // Malta
+    PL, // Polen
+    SI, // Slovenie
+    SK, // Slowakije
+    CZ, // Tsjechie
+    BG, // Bulgarije
+    RO, // Roemenie
+    HR  // Kroatie
+
+}
